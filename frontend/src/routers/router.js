cb5d100ddb64d5fff9b5791b2165b3870d66cc31
hyeondle <st.linsio@gmail.com>
2025-01-28 02:14:54 +0900

    component cache & page reset added

--- frontend/src/routers/router.js
+++ frontend/src/routers/router.js
@@ -46,22 +46,33 @@ export default class Router {
 
     async loadComponent(componentName) {
         try {
+            // 페이지 초기화
+            if (this.ObjectForDI.$page) {
+                this.ObjectForDI.$page.innerHTML = "";
+            }
+
+            // 캐싱된 컴포넌트가 있는 경우
             if (this.cache[componentName]) {
                 const cachedComponent = this.cache[componentName];
                 cachedComponent.renderSequnce(this.ObjectForDI.state);
                 return;
             }
 
+            // 동적으로 컴포넌트 로드
             const { default: Component } = await import(`../apps${componentName}.js`);
             const componentInstance = new Component(this.ObjectForDI);
 
+            // 컴포넌트를 캐싱
             this.cache[componentName] = componentInstance;
 
             componentInstance.renderSequnce(this.ObjectForDI.state);
-            
+
         } catch (error) {
             console.error(`Failed to load component: ${componentName}`, error);
-            this.$parent.innerHTML = `<p>Page not found</p>`;
+            if (this.ObjectForDI.$page) {
+                this.ObjectForDI.$page.innerHTML = `<p>Page not found</p>`;
+            }
         }
     }
+
 }
