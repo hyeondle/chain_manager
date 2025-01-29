a8a414e088c6caffeadf191ffd8a9ee687a6b80c
hyeondle <st.linsio@gmail.com>
2025-01-30 02:02:16 +0900

    multi language support

---
+++ frontend/src/styles/lang/franchisee.js
@@ -0,0 +1,29 @@
+const eeT = {
+	arrange: {
+		ko: "배치",
+		en: "Arrange",
+		jp: "配置",
+	},
+	menu: {
+		ko: "메뉴",
+		en: "Menu",
+		jp: "メニュー",
+	},
+	stock: {
+		ko: "재고",
+		en: "Stock",
+		jp: "在庫",
+	},
+	statics: {
+		ko: "통계",
+		en: "Statics",
+		jp: "統計",
+	},
+	setting: {
+		ko: "설정",
+		en: "Setting",
+		jp: "設定",
+	},
+};
+
+export default eeT;
